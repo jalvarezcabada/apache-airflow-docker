5bbea9f750e5738a07d90a0ed93a051ac45fb5f5
Joaquin Alvarez Cabada <110677560+jalvarezcabada@users.noreply.github.com>
2023-07-22 10:05:11 -0300

    Update dag_databricks.py

--- dags/dag_databricks.py
+++ dags/dag_databricks.py
@@ -4,36 +4,34 @@ from airflow.providers.databricks.operators.databricks import DatabricksSubmitRu
 
 default_args = {
     'owner': 'owner_name',
-    'start_date': datetime(2021, 10, 14), #Ejemplo: datetime(2021, 10, 14)
+    'start_date': datetime(2021, 10, 14),
     'email': ['email'],
     'email_on_failure': True,
-    # 'email_on_retry': True,
-    # 'retries': 1,
-    # 'retry_delay': timedelta(minutes=1)
+    'email_on_retry': True,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
 }
 
 @dag(
     dag_id='databricks',
-    schedule_interval='0 7 * * 1-5', #Ejemplo:'0 7 * * 1-5' (sumar 3 horas dado que esta en UTF)
+    schedule_interval='0 7 * * 1-5',
     default_args=default_args, 
-    description='descripcion',
+    description='<description>',
     catchup=False
 )
 
 def databricks_task():
 
-    #Definimos parametros sobre nuestro entorno de trabajo.
     notebook_task_params = {
-        'existing_cluster_id' : 'id_del_cluster',
+        'existing_cluster_id' : '<cluster_id>',
         'notebook_task' : {
-            'notebook_path': 'ruta_de_la_notebook',
+            'notebook_path': '<notebook_path>',
         },
     }
 
-    #Utilizamos operador de databricks para ejecutar nuestro script de manera remota.
     notebook_task = DatabricksSubmitRunOperator(
-        task_id='nombre_de_la_tarea',
-        databricks_conn_id='id_conexion',
+        task_id='<task_name>',
+        databricks_conn_id='conexion_id',
         json=notebook_task_params)
 
     notebook_task
